b5e81a1a3305121f9e3e500e8f50d46f65191a5b
lixiaowen <lixiaowen@sogou-inc.com>
2019-09-26 15:13:28 +0800

    增加路由

--- server/app.js
+++ server/app.js
@@ -4,44 +4,56 @@ import serve from 'koa-static';
 import historyApiFallback from 'koa2-history-api-fallback';
 import bodyparser from 'koa-bodyparser';
 import api from './routes/api.js';
-
+import koaWebpack from 'koa-webpack';
+import webpackConfig from '../webpack.conf'
 const app = new Koa();
-
-const PORT = process.env.HTTP_PORT || 4000;
-const IP = process.env.HTTP_IP || '0.0.0.0';
-// app.use(historyApiFallback());
-app.use(
-    serve(path.resolve(__dirname, '../dist'), {
-        maxage: 1000 * 60 * 60 * 24 * 30, // a month
-    }),
-);
-app.use(bodyparser());
-app.use(api());
-let koa2_timeout = async function (ctx, next) {
-    let tmr = null;
-    const timeout = 5 * 60 * 1000;// 设置超时时间
-    await Promise.race([
-        new Promise((resolve, reject) => {
-            tmr = setTimeout(() => {
-                let e = new Error('Request timeout');
-                e.status = 408;
-                reject(e);
-            }, timeout);
+main();
+async function main() {
+    const PORT = process.env.HTTP_PORT || 4000;
+    const IP = process.env.HTTP_IP || '0.0.0.0';
+    app.use(bodyparser());
+    app.use(api());
+    app.use(historyApiFallback({
+        htmlAcceptHeaders: ['text/html'],
+        index: '/'
+    }));
+    app.use(
+        serve(path.resolve(__dirname, '../dist'), {
+            maxage: 1000 * 60 * 60 * 24 * 30, // a month
         }),
-        new Promise((resolve, reject) => {
-            // 使用一个闭包来执行下面的中间件
-            (async function () {
-                await next();
-                clearTimeout(tmr);
-                resolve();
-            }());
-        })
-    ]);
-};
-app.use(koa2_timeout);
+    );
+    const middleware = await koaWebpack({
+        config: webpackConfig,
+        devMiddleware: {
+            stats: 'minimal'
+        }
+    });
+    app.use(middleware);
+    let koa2_timeout = async function (ctx, next) {
+        let tmr = null;
+        const timeout = 5 * 60 * 1000;// 设置超时时间
+        await Promise.race([
+            new Promise((resolve, reject) => {
+                tmr = setTimeout(() => {
+                    let e = new Error('Request timeout');
+                    e.status = 408;
+                    reject(e);
+                }, timeout);
+            }),
+            new Promise((resolve, reject) => {
+                // 使用一个闭包来执行下面的中间件
+                (async function () {
+                    await next();
+                    clearTimeout(tmr);
+                    resolve();
+                }());
+            })
+        ]);
+    };
+    app.use(koa2_timeout);
 
-app.listen(PORT, IP, () => {
-    console.log(`app started at http://${IP || 'localhost'}:${PORT}`);
-});
-
+    app.listen(PORT, IP, () => {
+        console.log(`app started at http://${IP || 'localhost'}:${PORT}`);
+    });
+}
 export default app;
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!-- <router-view/> -->
-    hello vue
+    hello vue2
   </div>
 </template>
 
